e48d6f21c437ad31b3b677917ba319bdbde8e894
Clara Wu <s.clara.wu@gmail.com>
2013-05-14 04:23:07 -0400

    keys

---
+++ diningphil/visualizations/secretkeys.py
@@ -0,0 +1,8 @@
+GMAIL_USERNAME = 's.clara.wu@gmail.com'
+GMAIL_PASSWORD = 'skk360Angelu$'
+
+LI_API_KEY= '2wvw62s4aonr'
+LI_SECRET_KEY = 'dzLJ1sCzRBwUaT6F'
+OAUTH_TOKEN = '13a896ac-d20f-43d4-8aa5-629b17910df0'
+OAUTH_SECRET = '268a5ce9-a9e2-4f2a-83e2-e89f448c6dc4'
+

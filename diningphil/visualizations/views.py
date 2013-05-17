dc1052a64b58b359adddd90cd8ee67be6c8d23af
Clara Wu <s.clara.wu@gmail.com>
2013-05-17 01:03:12 -0400

    bubble chart looks kickass

--- diningphil/visualizations/views.py
+++ diningphil/visualizations/views.py
@@ -141,57 +141,63 @@ def populate_db(request):
             research = student[6].strip()
             co = student[9].strip()
             startup = student[10].strip()
-            s, created = Student.objects.get_or_create(name = name, major = major, grad_year = grad_year)
-            s.timestamp = timestamp
-            s_location, created = City.objects.get_or_create(city_name = city)
-            s.location = s_location
-            s.position = position
-            if co != '':
-                url= "http://api.linkedin.com/v1/company-search:(companies:(name,universal-name,status,industries,employee-count-range,locations))?keywords=%s&count=1&start=0" % (co)
-                resp, content = client.request(url, "GET", headers=headers)
-                company = json.loads(content)
-                try:
-                    companies = company['companies']
-                    co_name = companies['values'][0]['name']
-                    co_type = companies['values'][0]['universalName']
-                    co_size = companies['values'][0]['employeeCountRange']['code']
-                    c, created = Company.objects.get_or_create(company_name = co_name, company_type = co_type, company_size = co_size)
-                    co_industries = companies['values'][0]['industries']['values']
-                    for i in co_industries:
-                        ind, created = Industries.objects.get_or_create(industry= i['name'])
-                        c.industries.add(ind)
-                    co_status = companies['values'][0]['status']['name']
-                    c.status = co_status
+            try:
+                s = Student.objects.get(name = name)
+            except Student.DoesNotExist:
+                s = Student(name = name, major = major, grad_year = grad_year)
+                s.timestamp = timestamp
+                s_location, created = City.objects.get_or_create(city_name = city)
+                s.location = s_location
+                s.position = position
+                if co != '':
+                    url= "http://api.linkedin.com/v1/company-search:(companies:(name,universal-name,status,industries,employee-count-range,locations))?keywords=%s&count=1&start=0" % (co)
+                    resp, content = client.request(url, "GET", headers=headers)
+                    company = json.loads(content)
                     try:
-                        locations = companies['values'][0]['locations']['values']
-                        for l in locations:
-                            postal = l['address']['postalCode']
-                            city = l['address']['city']
-                            loc, created = City.objects.get_or_create(city_name = city)
-                            if created:
-                                loc.postal_code = postal
-                                loc.save()
-                            c.locations.add(loc)
-                            c.save()
+                        companies = company['companies']
+                        co_name = companies['values'][0]['name']
+                        co_type = companies['values'][0]['universalName']
+                        co_size = companies['values'][0]['employeeCountRange']['code']
+                        c, created = Company.objects.get_or_create(company_name = co_name, company_type = co_type, company_size = co_size)
+                        co_industries = companies['values'][0]['industries']['values']
+                        for i in co_industries:
+                            ind, created = Industries.objects.get_or_create(industry= i['name'])
+                            c.industries.add(ind)
+                        co_status = companies['values'][0]['status']['name']
+                        c.status = co_status
+                        try:
+                            locations = companies['values'][0]['locations']['values']
+                            for l in locations:
+                                postal = l['address']['postalCode']
+                                city = l['address']['city']
+                                loc, created = City.objects.get_or_create(city_name = city)
+                                if created:
+                                    loc.postal_code = postal
+                                    loc.save()
+                                c.locations.add(loc)
+                                c.save()
+                        except:
+                            pass
+                        c.save()
+                        company_assigned = Company.objects.get(company_name=co_name)
+                        s.company = company_assigned
+                        s.save()
                     except:
                         pass
-                    c.save()
-                    company_assigned = Company.objects.get(company_name=co_name)
-                    s.company = company_assigned
-                    s.save()
-                except:
-                    pass
-            if startup != '':
-                s.startup = startup
-            if research != '':
-                s.research = research
-            s.save()
+                if startup != '':
+                    s.startup = startup
+                if research != '':
+                    s.research = research
+                s.save()
+            else:
+                pass
     return HttpResponse("It worked!")
 
 def companies(request):
     students = Student.objects.all()
     company_list = {'A' : {}, 'B' : {}, 'C' : {}, 'D' : {}, 'E' : {}, 'F' : {}, 'G' : {}, 'H' : {}, 'I' : {}}
-    other_list = {'OTHER' : {}}
+    research_list = {'research' : {}}
+    startup_list = {'startup' : {}}
     full_list = []
     for s in students:
         if s.company:
@@ -204,22 +210,49 @@ def companies(request):
                 company_list[size][company] = 1
         elif s.startup:
             startup = s.startup
-            if startup in other_list['OTHER']:
-                count2 = other_list['OTHER'][startup] + 1
-                other_list['OTHER'][startup] = count2
+            if startup in startup_list['startup']:
+                count2 = startup_list['startup'][startup] + 1
+                startup_list['startup'][startup] = count2
             else:
-                other_list['OTHER'][startup] = 1
+                startup_list['startup'][startup] = 1
         elif s.research:
             research = s.research
-            if research in other_list:
-                count3 = other_list['OTHER'][research] + 1
-                other_list['OTHER'][research] = count3
+            if research in research_list['research']:
+                count3 = research_list['research'][research] + 1
+                research_list['research'][research] = count3
             else:
-                other_list['OTHER'][research] = 1
-    full_list.append(other_list)
+                research_list['research'][research] = 1
+    full_list.append(startup_list)
+    full_list.append(research_list)
     full_list.append(company_list)
-    result = json.dumps(full_list)
+    result = convertToFlare(full_list)
     return HttpResponse(result)
+
+def convertToFlare(dictionary):
+    full_list =  dictionary
+    final_dict = {"name" : "flare", "children" : []}
+    for t in full_list:
+        if 'startup' in t:
+            startup_dict = {"name" : "Startup", "children" : []}
+            for key_s in t['startup']:
+                dict_entry = {"name" : key_s, "size" : t['startup'][key_s]}
+                startup_dict["children"].append(dict_entry)
+            final_dict["children"].append(startup_dict)
+        elif 'research' in t:
+            research_dict = {"name" : "Research", "children" : []}
+            for key_r in t['research']:
+                dict_entry = {"name" : key_r, "size" : t['research'][key_r]}
+                research_dict["children"].append(dict_entry)
+            final_dict["children"].append(research_dict)
+        else:
+            company_size = t.keys()
+            for key2 in company_size:
+                company_dict = {"name" : key2, "children" : []}
+                for key_c in t[key2]:
+                    dict_entry = {"name" : key_c, "size" : t[key2][key_c]}
+                    company_dict["children"].append(dict_entry)
+                final_dict["children"].append(company_dict)
+    return json.dumps(final_dict)
 
 
 def students(request):
@@ -237,5 +270,5 @@ def students(request):
     for key, value in dictionary_locations.iteritems():
         temp = [key,value]
         dictlist.append(temp)
-    result = json.dumps(dictlist);
+    result = json.dumps(dictlist)
     return HttpResponse(result)

3c0e570471b5ef89171a055422e9e1725a0550f1
Clara Wu <s.clara.wu@gmail.com>
2013-05-30 03:39:39 -0400

    changed css file, added line graph, bigger line, changed fonts

--- diningphil/media/js/scripts.js
+++ diningphil/media/js/scripts.js
@@ -51,105 +51,34 @@ function drawBubbles() {
   d3.select(self.frameElement).style("height", diameter + "px");
 }
 
-function drawUS() {
-  var w = 1280,
-    h = 800;
+function drawChange() {
+ var lineChartData = {
+			labels : ["Microsoft","Google","Facebook","Goldman Sachs","Palantir","Amazon","Research"],
+			datasets : [
+				{
+					fillColor : "rgba(220,220,220,0.7)",
+					strokeColor : "rgba(220,220,220,1)",
+					pointColor : "rgba(220,220,220,1)",
+					pointStrokeColor : "#fff",
+					data : [8,10,8,5,3,4,10]
+				},
+				{
+					fillColor : "rgba(151,187,205,0.7)",
+					strokeColor : "rgba(151,187,205,1)",
+					pointColor : "rgba(151,187,205,1)",
+					pointStrokeColor : "#fff",
+					data : [8,9,2,0,0,3,4]
+				}
+			]
 
-  var projection = d3.geo.azimuthal()
-      .mode("equidistant")
-      .origin([-98, 38])
-      .scale(1400)
-      .translate([640, 360]);
+		}
 
-  var path = d3.geo.path()
-      .projection(projection);
+	var myLine = new Chart(document.getElementById("change_time").getContext("2d")).Line(lineChartData); 
 
-  var svg = d3.select("#states_map").append("svg:svg")
-      .attr("width", w)
-      .attr("height", h);
-
-  var states = svg.append("svg:g")
-      .attr("id", "states");
-
-  var circles = svg.append("svg:g")
-      .attr("id", "circles");
-
-  var cells = svg.append("svg:g")
-      .attr("id", "cells");
-
-  d3.select("input[type=checkbox]").on("change", function() {
-    cells.classed("voronoi", this.checked);
-  });
-
-  d3.json("/media/js/us-states.json", function(collection) {
-    states.selectAll("path")
-        .data(collection.features)
-      .enter().append("svg:path")
-        .attr("d", path);
-  });
-
-  d3.csv("/media/js/flights-airport.csv", function(flights) {
-    var linksByOrigin = {},
-        countByAirport = {},
-        locationByAirport = {},
-        positions = [];
-
-    var arc = d3.geo.greatArc()
-        .source(function(d) { return locationByAirport[d.source]; })
-        .target(function(d) { return locationByAirport[d.target]; });
-
-    flights.forEach(function(flight) {
-      var origin = flight.origin,
-          destination = flight.destination,
-          links = linksByOrigin[origin] || (linksByOrigin[origin] = []);
-      links.push({source: origin, target: destination});
-      countByAirport[origin] = (countByAirport[origin] || 0) + 1;
-      countByAirport[destination] = (countByAirport[destination] || 0) + 1;
-    });
-
-    d3.csv("/media/js/airports.csv", function(airports) {
-
-      // Only consider cities with one path otherwise dont show them
-      airports = airports.filter(function(airport) {
-        if (countByAirport[airport.iata]) {
-          var location = [+airport.longitude, +airport.latitude];
-          locationByAirport[airport.iata] = location;
-          positions.push(projection(location));
-          return true;
-        }
-      });
-
-      // Compute the Voronoi diagram of cities projected positions.
-      var polygons = d3.geom.voronoi(positions);
-
-      var g = cells.selectAll("g")
-          .data(airports)
-        .enter().append("svg:g");
-
-      g.append("svg:path")
-          .attr("class", "cell")
-          .attr("d", function(d, i) { return "M" + polygons[i].join("L") + "Z"; })
-          .on("mouseover", function(d, i) { d3.select("#footer span").text(d.city); });
-
-      g.selectAll("path.arc")
-          .data(function(d) { return linksByOrigin[d.iata] || []; })
-        .enter().append("svg:path")
-          .attr("class", "arc")
-          .attr("d", function(d) { return path(arc(d)); });
-
-      circles.selectAll("circle")
-          .data(airports)
-        .enter().append("svg:circle")
-          .attr("cx", function(d, i) { return positions[i][0]; })
-          .attr("cy", function(d, i) { return positions[i][1]; })
-          .attr("r", function(d, i) { return Math.sqrt(countByAirport[d.iata]) * 10; })
-          .sort(function(a, b) { return countByAirport[b.iata] - countByAirport[a.iata]; });
-    });
-  });
 }
 
 function drawPie() {
-  var radius = 130,
+  var radius = 200,
     padding = 20;
 
   var color = d3.scale.ordinal()
@@ -157,7 +86,7 @@ function drawPie() {
 
   var arc = d3.svg.arc()
       .outerRadius(radius)
-      .innerRadius(radius - 60);
+      .innerRadius(radius - 90);
 
   var pie = d3.layout.pie()
       .sort(null)
@@ -296,7 +225,7 @@ function drawGraph() {
 }
 drawGraph();
 drawPie();
-drawUS();
+drawChange();
 drawBubbles();
 });
 
